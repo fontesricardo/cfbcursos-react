b829d2bf48f1beedff991ac7e3e5824aa8c6fb93
Ricardo Fontes de França <fontes.ricardo@hotmail.com>
2021-09-02 09:57:44 -0300

    Aula 10

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import React from 'react'
-import Relogio from './componentes/Relogio'
+import React, {useState} from 'react'
+import Numero from './componentes/Numero'
 import './App.css'
 
 export default function App(){
-  const textoDestaque={
-    color:'#00f',
-    fontSize:'3em'
+
+  const [num, setNum]=useState(10)
+
+  let n1=10
+
+  const n100=()=>{
+    n1=100
+    console.log(n1)
   }
 
   return(
     <>
-      <section className='caixa'>
-        <Relogio/>
-        <h1 style={{color:'#f00', fontSize:'5em'}}>CFB Cursos</h1>
-        <h2 style={textoDestaque}>Curso de React</h2>
-        <p className='texto'>Se inscreva em nosso canal e nos siga no instagram</p>  
-        <a href='https://www.youtube.com/cfbcursos' target='_blank' rel='noreferrer'>Clique Aqui!!!</a>
-      </section>
+      <p>Valor do state num: {num}</p>
+      <p>Valor do state n1: {n1}</p>
+      <Numero num={num} setNum={setNum}/>
     </>
   )
 }
