f6c817fce46ff1c01db875eb8884aee3cc672e65
Ricardo Fontes de França <fontes.ricardo@hotmail.com>
2021-09-12 09:36:13 -0300

    Aula 13 parte 2

--- src/App.js
+++ src/App.js
@@ -2,11 +2,18 @@ import React, { useState } from 'react'
 
 export default function App(){
 
-  const carros=['HRV','Golf','Focus','Cruze','Argo']
+  const carros=[
+    {categoria: "Esporte", preco: "110000.00", modelo: "Golf GTI"},
+    {categoria: "Esporte", preco: "120000.00", modelo: "Camaro"},
+    {categoria: "SUV", preco: "85000.00", modelo: "HRV"},
+    {categoria: "SUV", preco: "83000.00", modelo: "T-Cross"},
+    {categoria: "Utilitario", preco: "100000.00", modelo: "Hillux"},
+    {categoria: "Utilitario", preco: "90000.00", modelo: "Ranger"},
+  ]
 
   const listaCarros=carros.map(
     (c,i)=>
-      <li key={i}>{i} - {c}</li>
+      <li key={i}>{i} - {c.categoria} {c.modelo} - R$ {c.preco}</li>
   )
 
   return(
