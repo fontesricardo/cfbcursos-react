b16a42ee4b8fde6cd6dcaa373178db2af42572ce
Ricardo Fontes de França <fontes.ricardo@hotmail.com>
2021-09-12 19:49:20 -0300

    Aula 16 parte 2

--- src/App.js
+++ src/App.js
@@ -4,22 +4,31 @@ import Resultado from './componentes/Resultado'
 
 export default function App(){
 
-  const [nota1,setNota1]=useState(0)
-  const [nota2,setNota2]=useState(0)
-  const [nota3,setNota3]=useState(0)
-  const [nota4,setNota4]=useState(0)
+  const [notas,setNotas]=useState({"nota1":"0", "nota2":"0", "nota3":"0", "nota4":"0"})
+
+  const handleSetNotas=(e)=>{
+    if (e.target.getAttribute('name')=='fnota1'){
+      setNotas({"nota1":e.target.value, "nota2":notas.nota2, "nota3":notas.nota3, "nota4":notas.nota4})
+    }else if (e.target.getAttribute('name')=='fnota2'){
+      setNotas({"nota1":notas.nota1, "nota2":e.target.value, "nota3":notas.nota3, "nota4":notas.nota4})
+    }else if (e.target.getAttribute('name')=='fnota3'){
+      setNotas({"nota1":notas.nota1, "nota2":notas.nota2, "nota3":e.target.value, "nota4":notas.nota4})
+    }else if (e.target.getAttribute('name')=='fnota4'){
+      setNotas({"nota1":notas.nota1, "nota2":notas.nota2, "nota3":notas.nota3, "nota4":e.target.value})
+    }
+  }
 
   const somarNotas=()=>{
     console.log("Passou aqui")
-    return parseFloat(nota1) + parseFloat(nota2) + parseFloat(nota3) + parseFloat(nota4)
+    return parseFloat(notas.nota1) + parseFloat(notas.nota2) + parseFloat(notas.nota3) + parseFloat(notas.nota4)
   }
 
   return(
     <>
-      <Nota num={1} nota={nota1} setNota={setNota1}/>
-      <Nota num={2} nota={nota2} setNota={setNota2}/>
-      <Nota num={3} nota={nota3} setNota={setNota3}/>
-      <Nota num={4} nota={nota4} setNota={setNota4}/>
+      <Nota num={1} nota={notas.nota1} setNota={handleSetNotas}/>
+      <Nota num={2} nota={notas.nota2} setNota={handleSetNotas}/>
+      <Nota num={3} nota={notas.nota3} setNota={handleSetNotas}/>
+      <Nota num={4} nota={notas.nota4} setNota={handleSetNotas}/>
       <Resultado somaNotas={somarNotas()}/>
     </>
   )
